65223fd1a90ba4516fc3b4e9fa4a063a72853c5a
Umer siddique <sheikhumersiddique1999@gmail.com>
2023-11-07 00:24:23 +0500

    chakra basics

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,18 @@
+import { Container, Heading, Text } from "@chakra-ui/react";
+
 export default function Dashboard() {
   return (
-    <div>Dashboard</div>
-  )
+    <Container>
+      <Heading my="30px" p="10px">
+        Chakra UI Crash Course
+      </Heading>
+      <Text marginLeft="30px">Chakra ui test description</Text>
+      <Text color="blue.300">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus,
+        praesentium aperiam? Ipsam sequi velit labore accusamus pariatur. Totam,
+        aut minima dolorum, sequi sed perspiciatis sit, magni sint numquam eum
+        similique.
+      </Text>
+    </Container>
+  );
 }
